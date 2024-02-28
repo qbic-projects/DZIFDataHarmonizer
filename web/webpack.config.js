7943abd436016ddfae8187aedab40599dd2ec20a
Jannik Seidel <jannik.seidel@qbic.uni-tuebingen.de>
2024-02-28 09:36:00 +0100

    changes in the branding of the DataHarmonizer

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -18,21 +18,6 @@ module.exports = (env, argv) => {
       new CopyPlugin({
         patterns: [
           {
-            context: 'templates',
-            from: '**/*.pdf',
-            to: 'templates/[path][name][ext]',
-          },
-          {
-            context: 'templates',
-            from: '**/schema.yaml',
-            to: 'templates/[path][name][ext]',
-          },
-          {
-            context: 'templates',
-            from: '**/exampleInput/*',
-            to: 'templates/[path][name][ext]',
-          },
-          {
             from: 'main.html',
           },
         ],
